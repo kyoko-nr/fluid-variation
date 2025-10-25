a086040f270f577bd22e0f2f5d210b56d3591424
kyoko-nr <kyokokitagawa@me.com>
2025-10-25 11:23:53 +0900

    add tsconfig, remove unused code

--- packages/glsl/src/index.d.ts
+++ packages/glsl/src/index.d.ts
@@ -1,6 +1,4 @@
-export const addForce: string;
 export const advect: string;
 export const divergence: string;
 export const pressureJacobi: string;
 export const subtractGradient: string;
-export default addForce;
