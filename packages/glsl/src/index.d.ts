ab668e24b3cad5cfb73a7e774d6393ee19a25ecb
kyoko-nr <kyokokitagawa@me.com>
2025-10-25 11:08:56 +0900

    export glsl package

---
+++ packages/glsl/src/index.d.ts
@@ -0,0 +1,6 @@
+export const addForce: string;
+export const advect: string;
+export const divergence: string;
+export const pressureJacobi: string;
+export const subtractGradient: string;
+export default addForce;
--- packages/glsl/src/index.ts
+++ packages/glsl/src/index.ts
@@ -1,1 +1,6 @@
-export {};
+import advect from "./advect.glsl";
+import divergence from "./divergence.glsl";
+import pressureJacobi from "./pressureJacobi.glsl";
+import subtractGradient from "./subtractGradient.glsl";
+
+export { advect, divergence, pressureJacobi, subtractGradient };
